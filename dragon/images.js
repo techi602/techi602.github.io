ca4883913484dedd0768fadd1d45dd0e86e15e69
Michal Vrchota <michal.vrchota@gmail.com>
2019-03-30 15:17:17 +0100

    dragon

---
+++ dragon/images.js
@@ -0,0 +1,13 @@
+// Naètení obrázkù
+
+cela = new Image(117,27);
+cela.src = "img/cela.gif";
+
+zpet = new Image(117,27);
+zpet.src = "img/zpet.gif";
+
+cela2 = new Image(117,27);
+cela2.src = "img/cela2.gif";
+
+zpet2 = new Image(117,27);
+zpet2.src = "img/zpet2.gif";
