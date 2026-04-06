eb1d5be125d1a88de2f49a98b648a83a3d28f43d
Michal Vrchota <michal.vrchota@gmail.com>
2026-04-06 23:37:28 +0200

    homepage cleanup and modernize layout

--- scripts.js
+++ scripts.js
@@ -1,35 +1,37 @@
 
-(function($){
-    $.fn.shuffle = function() {
-        return this.each(function(){
-            var items = $(this).children();
-            return (items.length)
-            ? $(this).html($.shuffle(items))
-            : this;
-        });
+document.addEventListener('DOMContentLoaded', () => {
+    const iconsContainer = document.getElementById('icons');
+    if (iconsContainer) {
+        shuffle(iconsContainer);
     }
- 
-    $.shuffle = function(arr) {
-        for(
-            var j, x, i = arr.length; i;
-            j = parseInt(Math.random() * i),
-            x = arr[--i], arr[i] = arr[j], arr[j] = x
-        );
-        return arr;
-    }
-})(jQuery);
 
-$(document).ready(function () {
-    xshuffle();
+    const avatar = document.getElementById('avatar');
+    if (avatar) {
+        const originalSrc = 'images/avatar.jpg';
+        const hoverSrc = 'https://en.gravatar.com/userimage/17552033/c3fcd2d4827cf6d19b1075e70b35d1ae.jpg?size=100';
+        
+        avatar.addEventListener('mouseover', () => {
+            avatar.src = hoverSrc;
+        });
+        
+        avatar.addEventListener('mouseout', () => {
+            avatar.src = originalSrc;
+        });
+        
+        avatar.addEventListener('click', playMe);
+    }
 });
 
-function playMe()
-{
-    var snd = new Audio('sounds/' + Math.floor(Math.random() * 7) +'.wav');
-    snd.play();
+function playMe() {
+    const randomIndex = Math.floor(Math.random() * 7);
+    const snd = new Audio(`sounds/${randomIndex}.wav`);
+    snd.play().catch(e => console.error("Error playing sound:", e));
 }
 
-function xshuffle(ignore)
-{
-    $("#icons").shuffle();
+function shuffle(container) {
+    const elements = Array.from(container.children);
+    for (let i = elements.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        container.appendChild(elements[j]);
+    }
 }
